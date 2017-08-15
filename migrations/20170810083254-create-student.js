4d75143088fa8aef56f9df688a62f03cbb1a21dd
Rahmat Hidayat <email": null>
2017-08-15 18:14:41 +0700

    asd

--- migrations/20170810083254-create-student.js
+++ migrations/20170810083254-create-student.js
@@ -15,7 +15,8 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        unique: true
       },
       createdAt: {
         allowNull: false,
