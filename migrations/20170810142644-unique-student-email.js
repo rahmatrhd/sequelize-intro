11ef3ad78ae18888ee0772b92fcd78e90a11d6dd
Rahmat Hidayat <email": null>
2017-08-15 18:11:58 +0700

    asd

--- migrations/20170810142644-unique-student-email.js
+++ migrations/20170810142644-unique-student-email.js
@@ -2,26 +2,12 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return queryInterface.changeColumn('students', 'email', {
       type: Sequelize.STRING
     });
   },
 
   down: function (queryInterface, Sequelize) {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return queryInterface.changeColumn('students', 'email', {
       type: Sequelize.STRING
     })
